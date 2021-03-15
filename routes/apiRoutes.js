9127ec69649563dc72e0db9c1bd293ac7fd6bdd5
Eric Hurley <erichurley421@gmail.com>
2021-03-15 18:58:27 -0400

    Fixed left column click problem by installing uuid package and using writefilesync

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,16 @@
 const noteData = require('../db/db.json');
+const fs = require('fs');
+const uuid = require('uuid');
 
 module.exports = (app) => {
   app.get('/api/notes', (req, res) => res.json(noteData));
 
   app.post('/api/notes', (req, res) => {
+      const newId = uuid;
+      req.body['id'] = newId;
       noteData.push(req.body);
       res.json(true);
+      fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
   });
 
   app.delete('/api/notes', (req, res) => {
